df2ec3c4aed847ab3bb0a06c88c540c911f34e29
krishnaverma001 <krishnaverma2004.kv@gmail.com>
2025-05-16 00:32:42 +0530

    Don't Repeat yourself

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('update/<str:k>/', views.update, name='update'),
     path('delete/<str:k>/', views.delete, name='delete'),
-    path('toggle/<str:k>/', views.toggle, name='toggle')
+    path('toggle/<str:k>/', views.toggle, name='toggle'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from .forms import *
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TaskForm
+from .models import Task
 
 def index(request):
     tasks = Task.objects.all()
@@ -7,46 +8,39 @@ def index(request):
 
     if request.method == 'POST':
         form = TaskForm(request.POST)
-
         if form.is_valid():
             form.save()
-
-        return redirect('/')
+            return redirect('index')
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'index.html', context)
 
-
 def update(request, k):
-    task = Task.objects.get(id=k)
+    task = get_object_or_404(Task, id=k)
     form = TaskForm(instance=task)
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
-
         if form.is_valid():
             form.save()
-
-        return redirect('/')
+            return redirect('index')
 
     context = {'form': form}
     return render(request, 'update.html', context)
 
 def delete(request, k):
-    item = Task.objects.get(id=k)
-
+    item = get_object_or_404(Task, id=k)
     if request.method == 'POST':
         item.delete()
-        return redirect('/')
+        return redirect('index')
 
     context = {'item': item}
     return render(request, 'delete.html', context)
 
 def toggle(request, k):
-    task = Task.objects.get(id=k)
-
+    task = get_object_or_404(Task, id=k)
     if request.method == 'POST':
         task.complete = not task.complete
         task.save()
 
-    return redirect('/')
+    return redirect('index')
